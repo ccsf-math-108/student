c8f24d5ee7889a740956d2c242b27de4594e3166
shawnwiggins <swiggins@ccsf.edu>
2021-08-16 00:10:09 -0700

    push

---
+++ hw/hw06/student/tests/q2_4.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_4', 'points': None, 'suites': [{'cases': [{'code': '>>> CCSF_proportion == 0\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ hw/hw09/student/tests/q4_7.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q4_7',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> type(sample_size_calculation) == int\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
