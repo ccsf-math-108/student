9bd1b4d16597f433f254d5a12c805b783fbd415a
shawnwiggins <swiggins@ccsf.edu>
2021-08-20 14:05:43 -0700

    change view to none

--- hw/hw09/student/tests/q4_6.py
+++ hw/hw09/student/tests/q4_6.py
@@ -1,3 +1,3 @@
 test = {   'name': 'q4_6',
     'points': None,
-    'suites': [{'cases': [{'code': '>>> type(sample_mean_sd_statements) == np.ndarray\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'suites': [{'cases': [{'code': '>>> type(sample_mean_sd_statements) == np.ndarray\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
