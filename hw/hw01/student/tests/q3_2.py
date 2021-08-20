2ab3c7d7a12575a5cef3e552d81f6108ffd98226
shawnwiggins <swiggins@ccsf.edu>
2021-08-20 13:09:30 -0700

    re-run otter assign on q3

--- hw/hw01/student/tests/q3_2.py
+++ hw/hw01/student/tests/q3_2.py
@@ -1,1 +1,1 @@
-test = {'name': 'q3_2', 'points': None, 'suites': [{'cases': [{'code': '>>> 1 <= names_q2 <= 4\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {'name': 'q3_2', 'points': None, 'suites': [{'cases': [{'code': '>>> 1 <= names_q2 <= 4\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
