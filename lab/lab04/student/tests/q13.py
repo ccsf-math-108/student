74a28b6436f44a29165279ef777e440c9751daa5
shawnwiggins <swiggins@ccsf.edu>
2021-09-07 13:06:20 -0700

    update lab04 grader

--- lab/lab04/student/tests/q13.py
+++ lab/lab04/student/tests/q13.py
@@ -1,3 +1,3 @@
 test = {   'name': 'q13',
     'points': None,
-    'suites': [{'cases': [{'code': '>>> disemvowel("Datascience rules!") == "Dtscnc rls!"\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'suites': [{'cases': [{'code': '>>> disemvowel("Datascience rules!") == "Dtscnc rls!"\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ lab/lab04/student/tests/q5.py
@@ -0,0 +1,1 @@
+test = {'name': 'q5', 'points': None, 'suites': [{'cases': [{'code': '>>> submitted == True\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
