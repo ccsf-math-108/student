c296f06a67e229f844d418959f8006803ec4923d
shawnwiggins <swiggins@ccsf.edu>
2021-09-07 18:23:57 -0700

    remove quesiton 5

--- lab/lab04/student/tests/q5.py
+++
@@ -1,1 +0,0 @@
-test = {'name': 'q5', 'points': None, 'suites': [{'cases': [{'code': '>>> submitted == True\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
