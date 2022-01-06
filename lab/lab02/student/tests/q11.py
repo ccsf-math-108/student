03a905c3534e1f895f65601e493ad60a1a995fb3
shawnwiggins <22923434+shawnwiggins@users.noreply.github.com>
2022-01-06 11:56:18 -0800

    push lab02

---
+++ lab/lab02/student/tests/q11.py
@@ -0,0 +1,1 @@
+test = {'name': 'q11', 'points': 1, 'suites': [{'cases': [{'code': '>>> new_year == 2022\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
