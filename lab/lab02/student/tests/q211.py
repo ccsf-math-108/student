358157d45d7c7a93452c04f93ac6bc8cf1cc6211
shawnwiggins <22923434+shawnwiggins@users.noreply.github.com>
2022-01-18 02:20:38 -0800

    Start semester

--- lab/lab02/student/tests/q211.py
+++
@@ -1,7 +0,0 @@
-test = {   'name': 'q211',
-    'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> # Hint: You need to use math.sqrt and math.pi\n>>> import math\n>>> round(square_root_of_pi, 8) == 1.77245385\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
--- lab/lab10/student/tests/q3_2_2.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q3_2_2',
-    'points': None,
-    'suites': [{'cases': [{'code': '>>> np.isclose(prob_sick_given_positive, 0.3125)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
