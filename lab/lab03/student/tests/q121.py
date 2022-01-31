ef76891a328fe55b271da8c0708283baa96a44d3
shawnwiggins <22923434+shawnwiggins@users.noreply.github.com>
2022-01-31 09:00:17 -0800

    update lab03

--- lab/lab03/student/tests/q121.py
+++ lab/lab03/student/tests/q121.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q121',
     'points': None,
-    'suites': [   {   'cases': [{'code': '>>> abs(difference) == 30 or round(abs(difference), 1) == 30.0\n', 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> abs(difference) == 30 or round(abs(difference), 1) == 30.0\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
--- lab/lab03/student/tests/q131.py
+++ lab/lab03/student/tests/q131.py
@@ -1,1 +1,3 @@
-test = {'name': 'q131', 'points': None, 'suites': [{'cases': [{'code': '>>> sentence_length == 896\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'q131',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> sentence_length == 896\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
