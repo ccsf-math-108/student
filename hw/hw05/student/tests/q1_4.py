5dda96fcc540cb32ad8981d16b21a90cfc58a59a
Shawn Wiggins <22923434+shawnwiggins@users.noreply.github.com>
2024-01-19 18:26:00 +0000

    Remove previous content

--- hw/hw05/student/tests/q1_4.py
+++
@@ -1,7 +0,0 @@
-test = {   'name': 'q1_4',
-    'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> 0 <= ccsf_wins <= 10\nFalse', 'hidden': False, 'locked': False}, {'code': '>>> 0 <= ccsf_losses <= 10\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
