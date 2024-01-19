46365846ecd289a5d77d542497c1bf7c3b36000e
Shawn Wiggins <22923434+shawnwiggins@users.noreply.github.com>
2024-01-19 18:20:23 +0000

    Clear old materials

--- README.md
+++
@@ -1,3 +0,0 @@
-# materials-fa21
-
-This repo contains the materials for the Fall 2021 semester of MATH 108 at City College of San Francisco.
--- hw/hw02/student/tests/q1_1.py
+++
@@ -1,9 +0,0 @@
-test = {   'name': 'q1_1',
-    'points': None,
-    'suites': [   {   'cases': [   {   'code': '>>> np.allclose(weird_numbers, np.array([-2.,  1.4142135623730951, 3.,  9.738517742335421]), rtol=1e-03, atol=1e-03) == True\nTrue',
-                                       'hidden': False,
-                                       'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
